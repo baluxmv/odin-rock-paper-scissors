d8170d56b9ba75eb9764479423df1c8f32adeb88
Francisco <fmartinezv2002@gmail.com>
2024-06-29 12:21:52 -0400

    Implement functionality to play the game 5 times

--- scripts.js
+++ scripts.js
@@ -19,37 +19,57 @@ function getHumanChoice(){
 }
 
 
-function playRound(humanChoice, computerChoice){
-    if (humanChoice == "rock" && computerChoice == "paper"){
-        computerScore++;
+
+function playGame() {
+
+   
+    let humanScore = 0;
+    let computerScore = 0;
+    let winner;
+    for (let i = 0; i < 5; i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
-    else if (humanChoice == "rock" && computerChoice == "scissors"){
-        humanScore++;
+
+    if (humanScore > computerScore) {
+        winner = "human";
     }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        humanScore++;
+    else if (computerScore > humanScore) {
+        winner = "computer";
     }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
-        computerScore++;
+    else {
+        winner = "power of friendship";
     }
-    else if (humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore++;
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        computerChoice++;
-    }
-}
+    console.log(`The winner of this game was the ${winner}, which ended with 
+a score of ${humanScore} for the human and ${computerScore} for the computer.`)
 
 
-let humanScore = 0;
-let computerScore = 0;
+    function playRound(humanChoice, computerChoice){
+        if (humanChoice == "rock" && computerChoice == "paper"){
+            computerScore++;
+        }
+        else if (humanChoice == "rock" && computerChoice == "scissors"){
+            humanScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            computerChoice++;
+        }
+        console.log(`Human Choice: ${humanChoice} Computer Choice: ${computerChoice}`);
+        console.log(`Human Score: ${humanScore} Computer Score: ${computerScore}`);
+    }
+};
 
-const humanSelection = getHumanChoice();
-console.log(humanSelection);
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-playRound(humanSelection, computerSelection);
-console.log(humanScore, computerScore);
-
+// Start the game
+playGame();
 
 
