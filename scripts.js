520b89330a0483bff11336263d892cb48764770a
Francisco <fmartinezv2002@gmail.com>
2024-06-29 11:51:24 -0400

    Implement function to get human choice and initialize human and computer scores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 function getComputerChoice(){
     // Randomly get the values: 0, 1 or 2
     let computerChoice = Math.floor(Math.random() * 3);
@@ -14,4 +12,23 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let humanChoice = prompt("Rock, paper or scissors?");
+    return humanChoice;
+}
+
+
+function playRound(humanChoice, computerChoice){
+
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
