5d7875018faec68e61fc77c2bf7d83d1d187d8be
Francisco <fmartinezv2002@gmail.com>
2024-06-29 11:45:27 -0400

    Implement function that randomly returns a choice

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 console.log("Hello World");
 
+function getComputerChoice(){
+    // Randomly get the values: 0, 1 or 2
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0){
+        return "rock";
+    }
+    else if (computerChoice == 1){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
