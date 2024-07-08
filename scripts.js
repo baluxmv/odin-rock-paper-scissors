43484c30248f08011f03cf2afe44a56d9f2cfedd
Francisco <fmartinezv2002@gmail.com>
2024-07-08 12:44:24 -0400

    Add dinamically updating scores

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Function to get computer's choice
 function getComputerChoice(){
     // Randomly get the values: 0, 1 or 2
     let computerChoice = Math.floor(Math.random() * 3);
@@ -13,30 +14,9 @@ function getComputerChoice(){
 }
 
 
-let choicesButtons = document.querySelector(".choice-buttons");
-
-choicesButtons.addEventListener("click", (event) => {
-    let target = event.target;
-    
-    switch(target.id) {
-        case "rock-button":
-            playRound("rock", getComputerChoice());
-            break;
-        case "paper":
-            playRound("paper", getComputerChoice());
-            break;
-        case "scissors":
-            playRound("scissors", getComputerChoice());
-            break;
-    }
-});
-
-
-
-
+// Initialize scores
 let humanScore = 0;
 let computerScore = 0;
-let winner;
 
 
 function playRound(humanChoice, computerChoice){
@@ -58,21 +38,54 @@ function playRound(humanChoice, computerChoice){
     else if (humanChoice == "scissors" && computerChoice == "rock") {
         computerChoice++;
     }
+
+    // Log choices and scores to the console
     console.log(`Human Choice: ${humanChoice} Computer Choice: ${computerChoice}`);
     console.log(`Human Score: ${humanScore} Computer Score: ${computerScore}`);
+    
+    // Update the score display
+    document.querySelector("#player-score-number").textContent = humanScore;
+    document.querySelector("#computer-score-number").textContent = computerScore;
+
 
     if (humanScore == 5) {
         console.log('The human is the winner!');
-        humanScore = 0
-        computerScore = 0;
+        alert("The human is the winner!");
+        resetScores();
     }
     else if (computerScore == 5) {
         console.log('The computer is the winner!');
-        humanScore = 0;
-        computerScore = 0;
+        alert("The computer is the winner!");
+        resetScores();
     }
 }
 
 
+// Function to reset scores
+function resetScores() {
+    humanScore = 0;
+    computerScore = 0;
+    document.querySelector("#player-score-number").textContent = humanScore;
+    document.querySelector("#computer-score-number").textContent = computerScore;
+}
 
+
+// Event listener for button clicks
+let choicesButtons = document.querySelector(".choice-buttons");
+
+choicesButtons.addEventListener("click", (event) => {
+    let target = event.target;
+    
+    switch(target.id) {
+        case "rock-button":
+            playRound("rock", getComputerChoice());
+            break;
+        case "paper":
+            playRound("paper", getComputerChoice());
+            break;
+        case "scissors":
+            playRound("scissors", getComputerChoice());
+            break;
+    }
+});
 
